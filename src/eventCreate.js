14fafb8888e8f3d2f07669a24424cf8197f46033
Chris Banno <cbanno@my.bcit.ca>
2026-03-20 16:24:06 -0700

    Add additional form inputs for event creation

--- src/eventCreate.js
+++ src/eventCreate.js
@@ -8,30 +8,35 @@ async function submitEvent(e) {
     const address = document.getElementById("eventAddress").value;
     const team1 = document.getElementById("team1").value;
     const team2 = document.getElementById("team2").value;
+    
     const timeHour = document.getElementById("eventTimeHour").value;
     const timeMinute = document.getElementById("eventTimeMinute").value;
     const timeAMPM = document.getElementById("eventAMPM").value;
-
-    const isPublic = document.getElementById("eventPrivacy").checked;
-
+    
+    const partyPrivacy = document.getElementById("partyType").value;
+    
+    //combines 3 time values into one string
+    const eventTime = '${timeHour}:${timeMinute} ${timeAMPM}' ;
+    //combine the 2 teams into 1 venueName
+    const teamMatch = '${team1} VS ${team2}'
     try {
         const watchPartyRef = collection(db, "watch_parties");
 
         const docRef = await addDoc(watchPartyRef, {
             address: address,
             host: title,
-            team1: team1,
-            team2: team2,
-            timeHour: timeHour,
-            timeMinute: timeMinute,
-            isAM: timeAMPM,
-            isPublic: isPublic,
-            createdAt: serverTimestamp()
+            venueName: teamMatch,
+            time: eventTime,
+            partyType: partyPrivacy,
+            createdBy: auth.currentUser.uid,
+            createdAt: serverTimestamp(),
+            watchPartyID: docRef.id,
         });
+
         console.log("Event submitted with ID: ", docRef.id);
         alert("Event successfully listed!");
 
-        document.getElementById("eventForm").reset();
+        window.location.href = 'main.html?docID=${eventID}' ;
     } catch (error) {
         console.error("Error adding watch party: ", error);
         alert("Error: " + error.message);
