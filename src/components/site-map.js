c7edc43c260e2970939804166190402b8c003305
sjmy <smynott@my.bcit.ca>
2026-04-05 17:39:52 -0700

    Add watch party modal HTML to site-map.js

--- src/components/site-map.js
+++ src/components/site-map.js
@@ -24,7 +24,30 @@ class SiteMap extends HTMLElement {
             Lorem ipsum dolor sit amet consectetur adipisicing elit...
           </p>
         </div>
-      </div> -->`;
+      </div> -->
+      
+      <div class="modal fade" id="partyModal" tabindex="-1" aria-labelledby="partyModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="partyModalLabel">Watch Party Details</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <h3 id="modalHost"></h3>
+              <p id="modalTeams" class="fw-bold"></p>
+              <p id="modalAddress"></p>
+              <p id="modalTime"></p>
+              <hr>
+              <p>Placeholder Event Description</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-success">Join Party</button>
+            </div>
+          </div>
+        </div>
+      </div>`;
   }
 
   renderAuthControls() {
